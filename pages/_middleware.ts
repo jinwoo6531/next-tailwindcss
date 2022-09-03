f9d720ad93ce0e916f822793bccad1a7e6342096
jinwoo6531 <jintakk6531@gmail.com>
2022-09-03 21:58:29 +0900

    9/3 강의안료

--- pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (!req.url.includes("/api")) {
-    if (!req.url.includes("/auth/sign-in") && !req.cookies.token) {
-      return NextResponse.redirect("/auth/sign-in")
-    }
-  }
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
-import type { NextPage } from "next"
-import Layout from "../components/layout"
-import FloatingButton from "../components/floating-button"
-import Item from "../components/item"
-import useUser from "@libs/client/useUser"
-import useSWR from "swr"
-import { Product } from "@prisma/client"
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+import FloatingButton from '../components/floating-button';
+import Item from '../components/item';
+import useUser from '@libs/client/useUser';
+import useSWR from 'swr';
+// import { Product } from "@prisma/client"
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
 
 interface ProductsResponse {
-  ok: boolean
-  products: Product[]
+  ok: boolean;
+  products: Product[];
 }
 
 const Home: NextPage = () => {
-  const { user, isLoading } = useUser()
-  const { data } = useSWR<ProductsResponse>("/api/products")
+  const { data } = useSWR<ProductsResponse>('/api/products');
 
   return (
     <Layout title="홈" hasTabBar>
@@ -47,7 +52,7 @@ const Home: NextPage = () => {
         </FloatingButton>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
